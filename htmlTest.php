655e59b8e1e8a2917ec8bc76150fe43372211757
xyismy <xyismy@gmail.com>
2017-12-02 20:26:09 +0800

    s

---
+++ htmlTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xyismy
+ * Date: 17-12-2
+ * Time: 下午7:48
+ */
+
+class Test{
+    function html(){
+        $this->assgin('navigation','index');
+        $this->display('index.html');
+    }
+}
